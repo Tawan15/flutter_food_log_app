8ba1f3c7873ee80b7154a60c1ce0701e3bfe35d7
Tawan15 <s6619410021@sau.ac.th>
2026-03-25 12:16:06 +0700

    Done

--- lib/views/add_food_ui.dart
+++ lib/views/add_food_ui.dart
@@ -91,253 +91,139 @@ class _AddFoodUiState extends State<AddFoodUi> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.green,
+        backgroundColor: Colors.red,
         title: Text(
-          'กินกับหนู LOG',
+          'กินกัน LOG (เพิ่มรายการ)',
           style: TextStyle(
             color: Colors.white,
           ),
         ),
         centerTitle: true,
         leading: IconButton(
-          icon: Icon(Icons.arrow_back_ios, color: Colors.white),
           onPressed: () {
             Navigator.pop(context);
           },
+          icon: Icon(
+            Icons.arrow_back_ios_new_rounded,
+            color: Colors.white,
+          ),
         ),
       ),
       body: SingleChildScrollView(
-          child: Padding(
-              padding:
-                  EdgeInsets.only(top: 40, bottom: 50, left: 40, right: 40),
-              child: Center(
-                child: Column(
+        child: Padding(
+          padding: EdgeInsets.only(
+            top: 40,
+            bottom: 50,
+            left: 40,
+            right: 40,
+          ),
+          child: Center(
+            child: Column(
+              children: [
+                // ส่วนแสดง Logo
+                Image.asset(
+                  'assets/images/logo.png',
+                  width: 180,
+                  height: 180,
+                  fit: BoxFit.cover,
+                ),
+                SizedBox(height: 20),
+                // ป้อนกินอะไร
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'กินอะไร',
+                    style: TextStyle(
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+                TextField(
+                  controller: foodNameCtrl,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                    hintText: 'เช่น KFC, Pizza',
+                  ),
+                ),
+                SizedBox(height: 20),
+                // เลือกกินมื้อไหน
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'กินมื้อไหน',
+                    style: TextStyle(
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Image.asset(
-                      'assets/images/logo.png',
-                      width: 180,
-                      height: 180,
-                      fit: BoxFit.cover,
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    // ป้อนกินอะไร
-                    Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        'กินอะไร',
-                        style: TextStyle(
-                          fontSize: 18,
-                        ),
-                      ),
-                    ),
-                    TextField(
-                      controller: foodNameCtrl,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(5.0),
-                        ),
-                        hintText: 'เช่น KFC, Pizza',
-                      ),
-                    ),
-                    SizedBox(height: 20),
-                    Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        'กินมื้อไหน',
-                        style: TextStyle(
-                          fontSize: 18,
-                        ),
-                      ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        ElevatedButton(
-                          onPressed: () {
-                            setState(() {
-                              foodMeal = 'เช้า';
-                            });
-                          },
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor:
-                                foodMeal == 'เช้า' ? Colors.green : Colors.grey,
-                          ),
-                          child: Text(
-                            'เช้า',
-                            style: TextStyle(
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                        ElevatedButton(
-                          onPressed: () {
-                            setState(() {
-                              foodMeal = 'กลางวัน';
-                            });
-                          },
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: foodMeal == 'กลางวัน'
-                                ? Colors.green
-                                : Colors.grey,
-                          ),
-                          child: Text(
-                            'กลางวัน',
-                            style: TextStyle(
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                        ElevatedButton(
-                          onPressed: () {
-                            setState(() {
-                              foodMeal = 'เย็น';
-                            });
-                          },
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor:
-                                foodMeal == 'เย็น' ? Colors.green : Colors.grey,
-                          ),
-                          child: Text(
-                            'เย็น',
-                            style: TextStyle(
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                        ElevatedButton(
-                          onPressed: () {
-                            setState(() {
-                              foodMeal = 'ว่าง';
-                            });
-                          },
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor:
-                                foodMeal == 'ว่าง' ? Colors.green : Colors.grey,
-                          ),
-                          child: Text(
-                            'ว่าง',
-                            style: TextStyle(
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 20),
-                    Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        'กินไปเท่าไหร่',
-                        style: TextStyle(
-                          fontSize: 18,
-                        ),
-                      ),
-                    ),
-                    TextField(
-                      controller: foodPriceCtrl,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(5.0),
-                        ),
-                        hintText: 'เช่น 299 159',
-                      ),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    //กินกี่คน
-                    Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        'กินกันกี่คน',
-                        style: TextStyle(
-                          fontSize: 18,
-                        ),
-                      ),
-                    ),
-                    TextField(
-                      controller: foodPersonCtrl,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(5.0),
-                        ),
-                        hintText: 'เช่น 3 5',
-                      ),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    //กินไปวันไหน
-                    Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        'กินไปวันไหน',
-                        style: TextStyle(
-                          fontSize: 18,
-                        ),
-                      ),
-                    ),
-                    TextField(
-                      readOnly: true,
-                      controller: foodDateCtrl,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(5.0),
-                        ),
-                        hintText: 'เช่น 2020-01-31',
-                        suffixIcon: Icon(Icons.calendar_today),
-                      ),
-                      onTap: () {
-                        //เปิดปฎิทิน ให้ผู้ใช้เลือกแล้วเอามาแสดงที่ Textfield
-                        pickDate();
-                      },
-                    ),
-                    SizedBox(height: 20),
-                    //ปุ่มบันทึก
-                    ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.green,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(5.0),
-                        ),
-                        fixedSize: Size(
-                          MediaQuery.of(context).size.width,
-                          50,
-                        ),
-                      ),
-                      child: Text("บันทึก",
-                          style: TextStyle(
-                            color: Colors.white,
-                          )),
-                    ),
-                    SizedBox(height: 10),
-                    //ปุ่มยกเลิก
                     ElevatedButton(
                       onPressed: () {
                         setState(() {
-                          foodNameCtrl.clear();
-                          foodPriceCtrl.clear();
-                          foodPersonCtrl.clear();
-                          foodDateCtrl.clear();
                           foodMeal = 'เช้า';
-                          foodDate = null;
                         });
                       },
                       style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.red,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(5.0),
-                        ),
-                        fixedSize: Size(
-                          MediaQuery.of(context).size.width,
-                          50,
-                        ),
+                        backgroundColor:
+                            foodMeal == 'เช้า' ? Colors.green : Colors.grey,
                       ),
                       child: Text(
-                        "ยกเลิก",
+                        'เช้า',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          foodMeal = 'กลางวัน';
+                        });
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor:
+                            foodMeal == 'กลางวัน' ? Colors.green : Colors.grey,
+                      ),
+                      child: Text(
+                        'กลางวัน',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          foodMeal = 'เย็น';
+                        });
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor:
+                            foodMeal == 'เย็น' ? Colors.green : Colors.grey,
+                      ),
+                      child: Text(
+                        'เย็น',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          foodMeal = 'ว่าง';
+                        });
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor:
+                            foodMeal == 'ว่าง' ? Colors.green : Colors.grey,
+                      ),
+                      child: Text(
+                        'ว่าง',
                         style: TextStyle(
                           color: Colors.white,
                         ),
@@ -345,7 +231,123 @@ class _AddFoodUiState extends State<AddFoodUi> {
                     ),
                   ],
                 ),
-              ))),
+                SizedBox(height: 20),
+                // ป้อนกินไปเท่าไหร่
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'กินไปเท่าไหร่',
+                    style: TextStyle(
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+                TextField(
+                  controller: foodPriceCtrl,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                    hintText: 'เช่น 299.50',
+                  ),
+                ),
+                SizedBox(height: 20),
+                // ป้อนกินกันกี่คน
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'กินกันกี่คน',
+                    style: TextStyle(
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+                TextField(
+                  controller: foodPersonCtrl,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                    hintText: 'เช่น 3',
+                  ),
+                ),
+                SizedBox(height: 20),
+                // เลือกกินไปวันไหน
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'กินไปวันไหน',
+                    style: TextStyle(
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+                TextField(
+                  controller: foodDateCtrl,
+                  readOnly: true,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                    hintText: 'เช่น 2020-01-31',
+                    suffixIcon: Icon(Icons.calendar_today),
+                  ),
+                  onTap: () {
+                    //เปิดปฏิทิน ให้ผู้ใช้เลือกแล้วเอามาแสดงที่ TextField นี้
+                    pickDate();
+                  },
+                ),
+                SizedBox(height: 20),
+                // ปุ่มบันทึก
+                ElevatedButton(
+                  onPressed: () {
+                    saveFood();
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                    fixedSize: Size(
+                      MediaQuery.of(context).size.width,
+                      50,
+                    ),
+                  ),
+                  child: Text(
+                    "บันทึก",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+                // ปุ่มยกเลิก
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.red,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                    fixedSize: Size(
+                      MediaQuery.of(context).size.width,
+                      50,
+                    ),
+                  ),
+                  child: Text(
+                    "ยกเลิก",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
